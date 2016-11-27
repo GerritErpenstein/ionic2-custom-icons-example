2f71aa84508cde7c8139c5bf921654fab0deea90
Gerrit Erpenstein <gerrit.erpenstein.dev@gmail.com>
2016-11-27 13:46:22 +0100

    updated to ionic 2.0.0-rc.3 and @ionic/app-scripts 0.0.45

--- config/sass.config.js
+++ config/sass.config.js
@@ -9,6 +9,7 @@ module.exports = {
    includePaths: [
       'node_modules/ionic-angular/themes',
       'node_modules/ionicons/dist/scss',
+      'node_modules/ionic-angular/fonts',
       'node_modules/ionic2-custom-icons/directive/scss/',
       '.tmp-custom-icons/scss/'
    ],
